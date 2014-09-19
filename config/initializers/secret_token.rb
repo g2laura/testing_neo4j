f0d9a87a49b1f4ea7b057c1607ecd1454923374a
Laura Gomez <laura0703@gmail.com>
2014-09-19 16:27:48 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Neo4jtest::Application.config.secret_key_base = '484d17d9701f9a3e04bd3cd28ae9a886fe9b1d7c1ef4babc10c34f94c5c252f7d44c9b73546d4a9d3f872c0c0174c6a5f82f98e2b7545ef8329e67696b319575'
