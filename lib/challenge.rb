4f3fe4ab17345018891554dc091deefea50ae6fa
Laura Gomez <laura0703@gmail.com>
2014-09-22 13:57:33 -0500

    Add validation if the node exists

--- lib/challenge.rb
+++ lib/challenge.rb
@@ -3,10 +3,9 @@ require 'rubygems'
 require 'bundler/setup'
 require 'neo4j'
 require 'parallel'
+require 'pry'
 
 class Challenge
-
-  include Neo4j::ActiveNode
 
   @@errors = ActiveModel::Errors.new(self)
   @@file = File.expand_path('../users.csv', __FILE__)
@@ -16,15 +15,20 @@ class Challenge
     value.match(/^\d*$/) ? true : (@@errors.add('Not a number', 'The value #{value} is not a number.') ; false)
   end
 
-  def self.create_node (type, value)
-    valid?(value) ? Neo4j::Node.create(user: value) : nil
+  def self.create_node (value)
+    if valid?(value)
+      node = @@session._query("MATCH (n {user: '#{value}'}) RETURN ID(n)").data.first
+      node.nil? ? Neo4j::Node.create(user: value) : Neo4j::Node.load(node["row"].first.to_i)
+    else
+      nil
+    end
   end
 
-  Parallel.each(CSV.read(@@file), :in_threads => 10) do |user_row|
+  Parallel.each(CSV.read(@@file), :in_threads => 1) do |user_row|
     user_a_id, user_b_id = user_row
     Neo4j::Transaction.run do 
-      a = create_node('a', user_a_id.delete(' '))
-      b = create_node('b', user_b_id.delete(' '))
+      a = create_node(user_a_id.delete(' '))
+      b = create_node(user_b_id.delete(' '))
       unless (a.blank? || b.blank?)
         a.create_rel(:relationship, b)
         b.create_rel(:relationship, a)
