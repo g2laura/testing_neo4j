f710370d9561b9885a2326f7bbb1ca512ea8d308
Laura Gomez <laura0703@gmail.com>
2014-09-22 14:35:24 -0500

    Add processes instead threads

--- lib/challenge.rb
+++ lib/challenge.rb
@@ -15,7 +15,7 @@ class Challenge
     value.match(/^\d*$/) ? true : (@@errors.add('Not a number', 'The value #{value} is not a number.') ; false)
   end
 
-  def self.create_node (value)
+  def self.find_or_create_node (value)
     if valid?(value)
       node = @@session._query("MATCH (n {user: '#{value}'}) RETURN ID(n)").data.first
       node.nil? ? Neo4j::Node.create(user: value) : Neo4j::Node.load(node["row"].first.to_i)
@@ -24,11 +24,11 @@ class Challenge
     end
   end
 
-  Parallel.each(CSV.read(@@file), :in_threads => 1) do |user_row|
+  Parallel.each(CSV.read(@@file), :in_processes => 1) do |user_row|
     user_a_id, user_b_id = user_row
     Neo4j::Transaction.run do 
-      a = create_node(user_a_id.delete(' '))
-      b = create_node(user_b_id.delete(' '))
+      a = find_or_create_node(user_a_id.delete(' '))
+      b = find_or_create_node(user_b_id.delete(' '))
       unless (a.blank? || b.blank?)
         a.create_rel(:relationship, b)
         b.create_rel(:relationship, a)
